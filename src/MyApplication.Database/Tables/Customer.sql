86cc067f372e3bfe88c1d4e8a5f933cf3b35751c
Kade Williams <kade.williams@northwestern.edu>
2026-08-06 12:36:18 -0500

    starting over

--- src/MyApplication.Database/Tables/Customer.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[Customer]
-(
-    [CustomerId] INT           NOT NULL IDENTITY(1,1),
-    [FirstName]  NVARCHAR(100) NOT NULL,
-    [LastName]   NVARCHAR(100) NOT NULL,
-    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([CustomerId])
-);
